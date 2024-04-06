df6c91e0e61684662cdf05484b94afc1d496f0c8
Nas16iara <Ranasiaclark010@gmail.com>
2024-04-06 11:10:03 -0400

    3rd part done - functionality added to frontenf

--- frontend/src/components/sidebar/Conversation.jsx
+++ frontend/src/components/sidebar/Conversation.jsx
@@ -1,27 +1,40 @@
-const Converstation = () => {
+import useConversations from '../../store/useConversation.js'
+
+const Conversation = ({conversation,lastIdx,emoji}) => {
+    const {selectedConversation, setSelectedConversation} = useConversations();
+
+    const isSelected = selectedConversation?._id === conversation._id;
+
   return <>
-      <div className="flex gap-2 items-center hover:bg-indigo-500 rounded p-2 py-1 cursor-pointer">
+      <div className={`flex gap-2 items-center hover:bg-indigo-500 rounded p-2 py-1 cursor-pointer
+        ${isSelected ? "bg-indigo-500" : ""}
+      `}
+        onClick={() => setSelectedConversation(conversation)}
+      >
         <div className="avatar online">
             <div className="w-12 rounded-full">
-                <img src="https: //cdn0.iconfinder.com/data/icons/communication-line-10/24/account_profile_user_contact_person_avatar_placeholder-512.png" alt="user avatar"></img>
+                <img 
+                    src={conversation.profilePic} 
+                    alt="user avatar"
+                />
             </div>
         </div>
         <div className="flex flex-col flex-1">
             <div className="flex gap-3 justify-between">
-                <p className="font-bold text-gray-200">John Doe</p>
-                <span className="text-xl">🎃</span>
+                <p className="font-bold text-gray-200">{conversation.fullName}</p>
+                <span className="text-xl">{emoji}</span>
             </div>
         </div>
     </div>
-    <div className=" fivider my-0 py-0 h-1"/>
+    {!lastIdx && <div className=" fivider my-0 py-0 h-1"/>}
   </>;
 }
 
-export default Converstation;
+export default Conversation;
 
 
 // STARTER CODE 
-// const Converstation = () => {
+// const conversation = () => {
 //     return <>
 //         <div className="flex gap-2 items-center hover:bg-indigo-500 rounded p-2 py-1 cursor-pointer">
 //           <div className="avatar online">
@@ -40,4 +53,4 @@ export default Converstation;
 //     </>;
 //   }
   
-//   export default Converstation;
+//   export default conversation;
