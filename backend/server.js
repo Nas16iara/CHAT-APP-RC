6181da05f2187b121d04386395857749ce04cffb
Nas16iara <Ranasiaclark010@gmail.com>
2024-04-06 21:40:57 -0400

    updating server

--- backend/server.js
+++ backend/server.js
@@ -16,8 +16,7 @@ const __dirname = path.resolve();
 
 dotenv.config();
 
-
-app.use(express.json()); // to parse the incoming request with JSON payloads (from req.body)
+app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
@@ -26,12 +25,11 @@ app.use("/api/users", userRoutes);
 
 app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-app.get("*", (req,res) => {
-  req.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
 });
 
-server.listen(5000, () => {
-    connectToMongoDB();
-    console.log(`Server Running on port ${PORT}`);
-
+server.listen(PORT, () => {
+	connectToMongoDB();
+	console.log(`Server Running on port ${PORT}`);
 });
